3e11cb1de5b23f1f92ad52a4128d4863abef35ad
Hariharan <jhariharan2004@gmail.com>
2024-02-19 12:07:16 +0530

    Set up basic Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello world")
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import "dotenv/config"
+
+const app = express()
+const PORT = 3000
+
+
+
+// middleware
+
+app.use(cookieParser())
+app.use(express.static('public'))
+
+
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world!</h1>")
+})
+
+
+
+
+// start app
+
+app.listen(PORT, () => {
+    console.log("Listening...")
+})
