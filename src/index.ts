9f2118ce036060329882f0262b58bf53d3244fd3
Hariharan <jhariharan2004@gmail.com>
2024-02-19 12:36:53 +0530

    Set up express-handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import "dotenv/config"
+import { engine } from 'express-handlebars'
 
 const app = express()
 const PORT = 3000
@@ -14,10 +15,18 @@ app.use(express.static('public'))
 
 
 
+
+// handlebars
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', 'views')
+
+
 // routes
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello world!</h1>")
+    res.render('home')
 })
 
 
